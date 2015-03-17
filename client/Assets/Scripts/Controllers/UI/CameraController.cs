db715815fa12453f3ab2889d631cbef086fff747
Eric Liu <eric.yuhui.liu@gmail.com>
2015-03-17 13:38:34 -0400

    added camera keyboard controls

---
+++ client/Assets/Scripts/Controllers/UI/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Wardraft.UI {
+
+  public class CameraController : MonoBehaviour {
+  
+    CameraViewModel CVM;
+    Vector2         velocity;
+    
+    void Start () {
+      CVM = gameObject.GetComponent<CameraViewModel>();
+      velocity = new Vector2(0f,0f);
+    }
+    
+    public void SetVelocity (float x, float y) {
+      velocity = new Vector2(x,y);
+    }
+    
+    void Update () {
+      move();
+    }
+    
+    //Move according to velocity
+    void move () {
+      if (velocity.x < 0) CVM.MoveLeft();
+      if (velocity.x > 0) CVM.MoveRight();
+      if (velocity.y < 0) CVM.MoveBack();
+      if (velocity.y > 0) CVM.MoveForward();
+    }
+  
+  }
+
+}
---
+++ client/Assets/Scripts/ViewModel/UI/CameraViewModel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Wardraft.UI {
+
+  public class CameraViewModel : MonoBehaviour {
+  
+    Transform     container;
+    public float  speed   = 20f;
+    
+    void Start () {
+      container = transform.parent;
+    }
+    
+    public void MoveForward () { move(container.forward); }
+    
+    public void MoveBack () { move(container.forward * -1f); }
+    
+    public void MoveLeft () { move(container.right * -1f); }
+    
+    public void MoveRight () { move(container.right); }
+    
+    void move (Vector3 dir) {
+      transform.Translate(dir * Time.deltaTime * speed, Space.World);
+    }
+  
+  }
+
+}
